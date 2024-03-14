3566bac4a3db8c52a4aa2df142faa5eb38a0cc7e
SeattleDude <lilrainier1@gmail.com>
2024-03-13 18:10:56 -0600

    Encryption update

--- client.py
+++ client.py
@@ -1,11 +1,21 @@
-import socket, threading, os
+import socket, threading, os, base64
+from cryptography import fernet
+from cryptography.hazmat.primitives import hashes
 
-# VERSION: 2.0
-versionFloat = "2.0"
+# VERSION: 3.0
+versionFloat = "3.0"
+print(f"*****|CLIENT VERSION: {versionFloat}|*****")
+
 
 # ideally these would be setup by arguments or some sort of input system
 serverIP = input("Server IP: ") or socket.gethostbyname(socket.gethostname())
 serverPort = int(input("Server Port: ") or 5000)
+password = input("Server Password: ") # switch input with a hidden input library
+
+hashDigest = hashes.Hash(hashes.SHA256())
+hashDigest.update(password.encode())
+hashedPassword = base64.urlsafe_b64encode(hashDigest.finalize())
+
 
 serverAddr = (serverIP, serverPort)
 DisconnMsg = '' # we will fill this with the first message the server sends (to keep the disconnect messages the same between versions)
@@ -29,35 +39,38 @@ def send():
         try:
             message = input(">").encode()
             if message:
+                key = fernet.Fernet(hashedPassword)
+                encryptedMessage = key.encrypt(message)
                 if message.decode() == DisconnMsg:
-                    MyInterface.send(DisconnMsg.encode())
+                    encryptedDisconn = key.encrypt(DisconnMsg)
+                    MyInterface.send(encryptedDisconn)
                     os._exit(0)
                 else:
-                    MyInterface.send(message)
+                    MyInterface.send(encryptedMessage)
         except:
             print("The server probably shutdown!")
             os._exit(0)
     
 def recieve():
     global DisconnMsg
+    key = fernet.Fernet(hashedPassword)
     while True:
         try:
-            message = MyInterface.recv(2048).decode()
-            if message:
+            EncMessage = MyInterface.recv(2048)
+            if EncMessage:
+
+                message = key.decrypt(EncMessage).decode()
+
                 if message == "NICK":
-                    MyInterface.send(nickname.encode()) # move this and the input field to its own function in case we need to re query from the server side
+                    MyInterface.send(key.encrypt(nickname.encode())) # move this and the input field to its own function in case we need to re query from the server side
                 elif message == "ESCAPESEQUENCE":
-                    DisconnMsg = MyInterface.recv(2048).decode()
+                    DisconnMsg = key.decrypt(MyInterface.recv(2048)).decode()
                     print(f"\bDisconnect with: \"{DisconnMsg}\"", end="\n>")
                 else:
                     print("\b" + message, end="\n>") # delete the little ">" print the message and print a new line with the ">" for user input again
         except:
             pass
 
-def TellVersion():
-    print(f"*****|CLIENT VERSION: {versionFloat}|*****")
-
-TellVersion()
 nickname = input("Enter a nickname> ") # make user choose a nickname so their IP isn't directly sent to other users
 
 sendThread = threading.Thread(target=send)
--- server.py
+++ server.py
@@ -1,13 +1,23 @@
-import socket, threading
+import socket, threading, base64
+from cryptography import fernet
+from cryptography.hazmat.primitives import hashes
 
-# VERSION: 3.0
-versionFloat = "3.0"
-
+# VERSION: 4.0
+versionFloat = "4.0"
+print(f"*****|SERVER VERSION: {versionFloat}|*****")
 
 serverIP = socket.gethostbyname(socket.gethostname())
 serverPort = 5000 # making this standard is easier than syncing over the network between server and client
 serverAddr = (serverIP, serverPort)
 DisconnMsg = "ESC!!" # server owner can customize this
+
+# need to implement server side protection for buffer overflow to crash the server with input
+bufferSize = 4096 # buffer size for input, can be overflowed and crash the server
+
+password = input("Set a secure password> ") or "password"
+digest = hashes.Hash(hashes.SHA256())
+digest.update(password.encode())
+hashedPassword = base64.urlsafe_b64encode(digest.finalize())
 
 
 listenInterface = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,26 +34,42 @@ nicknameList = []
 #TODO: implement a chat history so we can administer the server and send the chat logs to new users so they can get the FULL conversation history
 # this may not be a good idea for user trust tho
 
+def EncSend(connection,  message):
+    try:
+        message = message.encode()
+    except:
+        pass
+    key = fernet.Fernet(hashedPassword)
+    encryptedMessage = key.encrypt(message)
+    connection.send(encryptedMessage)
+
 # we try not to broadcast the users IP so they can try to stay as anonymous as possible
 def broadcast(message, preamble): # sends message to ALL clients in clientList (should be connected)
     if type(message) != bytes():
         message = message.encode()
     for client in clientList:
         try:
+            key = fernet.Fernet(hashedPassword)
+            encryptedMessage = key.encrypt(message)
             if client == preamble:
                 continue
             else:
-                client.send(message)
+                client.send(encryptedMessage)
         except:
-            print(client)
+            # print(client)
+            print("broadcastFuncErr!")
 
 def clientMsg(client, address, nickName): # takes client parameter; type = connection object
+    key = fernet.Fernet(hashedPassword)
     formattedAddr = f"{address[0]}:{address[1]}"
     alive = True
     while alive:
         try:
-            msg = bytes(client.recv(2048)).decode() # try to recieve data
-            if msg:
+            EncMsg = bytes(client.recv(bufferSize)) # try to recieve data
+            if EncMsg:
+
+                msg = key.decrypt(EncMsg).decode()
+
                 if msg == DisconnMsg:
                     clientList.remove(client)
                     client.close()
@@ -57,22 +83,34 @@ def clientMsg(client, address, nickName): # takes client parameter; type = conne
                     broadcast(f"{nickName}: {msg}", client) # in fString could be formattedAddr or userNickName
                     print(f"({formattedAddr}--{nickName}): {msg}")
         except OSError: # handle dirty disconnects from the client (client terminal closed)
-            print(f"{address[0]}:{address[1]} Probable Disconnect!")
+            broadcast(f"{nickName} Disconnected!", client)
+            print(f"({formattedAddr}--{nickName}) Probable Disconnect!") # we cant be sure if it was an intentional disconnect, in any case, the client is no longer connected
             clientList.remove(client)
             nicknameList.remove(nickName)
             alive = False
             break
 
 def recieve():
+    key = fernet.Fernet(hashedPassword)
     while True:
         conn, addr = listenInterface.accept()
-        conn.send('NICK'.encode()) # query user for nickname
-        userNickName = conn.recv(2048).decode()
-        conn.send('ESCAPESEQUENCE'.encode()) # sync the userSide disconn message
-        conn.send(DisconnMsg.encode())
+        formattedAddr = f"{addr[0]}:{addr[1]}"
+        EncSend(conn, 'NICK'.encode())
+        # conn.send('NICK'.encode()) # query user for nickname
+        # print(conn.recv(bufferSize).decode())
+        try:
+            userNickName = key.decrypt(conn.recv(bufferSize)).decode()
+        except:
+            # the password wasn't entered correctly!
+            print(f"Failed auth attempt! -> {formattedAddr}")
+            continue
+        # userNickName = conn.recv(bufferSize).decode()
+        EncSend(conn, 'ESCAPESEQUENCE'.encode())
+        # conn.send('ESCAPESEQUENCE'.encode()) # sync the userSide disconn message
+        EncSend(conn, DisconnMsg.encode())
+        # conn.send(DisconnMsg.encode())
         clientList.append(conn)
         nicknameList.append(userNickName)
-        formattedAddr = f"{addr[0]}:{addr[1]}"
         broadcast(f"{userNickName} connected!", conn) # in fString could be formattedAddr or userNickName
         # print all the existing connections like a current user list so the user knows who is in the room
         print(f"({formattedAddr}--{userNickName}) connected!")
@@ -80,9 +118,7 @@ def recieve():
         handleThread = threading.Thread(target=clientMsg, args=(conn,addr, userNickName))
         handleThread.start()
 
-def tellVersion():
-    print(f"*****|SERVER VERSION: {versionFloat}|*****")
-
-tellVersion()
 print(f"server is listening on: {serverIP}:{serverPort}")
-recieve()
+# moved the starting method to a thread so later on I can make a server admin UI or terminal input for starting and stopping the server etc...
+recieveThread = threading.Thread(target=recieve)
+recieveThread.start()
